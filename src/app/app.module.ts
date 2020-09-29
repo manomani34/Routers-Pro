091c5f7f304dcab5e226b9cd08b026e31f129734
manomani34 <59196010+manomani34@users.noreply.github.com>
2020-09-29 08:04:36 +0330

    oFFice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AccuntsComponent } from './accunts/accunts.component';
 import { UsersComponent } from './users/users.component';
-import { RouterModule, Routes } from '@angular/router';
 import { EditUserComponent } from './users/edit-user/edit-user.component';
 import { UserComponent } from './users/user/user.component';
-import { EditAccountComponent } from './accunts/edit-account/edit-account.component';
 import { UserService } from './users/user.service.service';
 import { FormsModule } from '@angular/forms';
+import { LoginServiceService } from './login-service.service';
+import { AuthGuardService } from './auth-guard.service';
 
 
-const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'users', component: UsersComponent , children: [
-  {path:':id', component: UserComponent},  
-  {path:':id/edit', component: EditUserComponent}
-  ]},    
-  // {path:'users/:id/:name', component: UserComponent},
-  {path:'accounts', component: AccuntsComponent},
-  {path:'accounts/:id/edit', component: EditAccountComponent}
-];
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,16 +24,15 @@ const routes: Routes = [
     UsersComponent,
     EditUserComponent,
     UserComponent
-    
+
   ],
   imports: [
     FormsModule,
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes)
+    BrowserModule,  
+    AppRoutingModule
   ],
   // provide services 
-  providers: [UserService],
+  providers: [UserService, AuthGuardService, LoginServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../login-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginnservice: LoginServiceService) { }
 
   ngOnInit(): void {
   }
 
+  onLogin(){
+    this.loginnservice.login();
+  }
+  
+  onLogout(){
+this.loginnservice.logout();
+  }
 }
